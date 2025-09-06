172b4a50644b4b9ff5de7c8c5b512497b679dc0e
YaNaRion <druet.goldorak@gmail.com>
2025-09-06 13:23:55 -0400

    top bar fonctionnelle

--- router/router.go
+++ router/router.go
@@ -7,6 +7,12 @@ import (
 	"net/http"
 )
 
+// Form template
+type FromName struct {
+	Fname string `json:"fname"`
+	Lname string `json:"lname"`
+}
+
 type Router struct {
 	Mux       *http.ServeMux
 	templates *Templates
@@ -22,6 +28,8 @@ type Templates struct {
 	templates *template.Template
 }
 
+var ErrorFrom string = "MAUVAIS NOM D'UTILISATEUR OU MDP"
+
 func newTemplate() *Templates {
 	return &Templates{templates: template.Must(template.ParseGlob("views/*.html"))}
 }
@@ -36,16 +44,28 @@ func (t *Templates) Render(w io.Writer, name string, data any) error {
 func Setup(mux *http.ServeMux) *Router {
 	router := newRouter(mux)
 
+	// Serve static file
 	router.Mux.Handle(
 		"/assets/",
 		http.StripPrefix("/assets/", http.FileServer(http.Dir("./views/assets/"))),
 	)
 
+	// Post htmx temps
 	router.Mux.HandleFunc("POST /count", router.handleClick)
-	router.Mux.HandleFunc("/admin", router.routeAdmin)
 	router.Mux.HandleFunc("POST /admin-connection", router.checkAdminConnection)
+
+	// Route Home
 	router.Mux.HandleFunc("/home", router.routeHome)
+
+	// Route pour les pages
+	router.Mux.HandleFunc("/admin-content", router.routeAdmin)
+	router.Mux.HandleFunc("/home-content", router.routeHomeContent)
+	router.Mux.HandleFunc("/books-content", router.routeBooksContent)
+	router.Mux.HandleFunc("/series-content", router.routeSeriesContent)
+
+	// Route par defaut
 	router.Mux.HandleFunc("/", router.rerouteToHome)
+
 	return router
 }
 
@@ -57,23 +77,10 @@ func newRouter(mux *http.ServeMux) *Router {
 	}
 }
 
-type FromName struct {
-	Fname string `json:"fname"`
-	Lname string `json:"lname"`
-}
-
-var ErrorFrom string = "MAUVAIS NOM D'UTILISATEUR OU MDP"
-
-func (rt *Router) rerouteToHome(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
-}
-
 func (rt *Router) checkAdminConnection(w http.ResponseWriter, r *http.Request) {
 	var data FromName
 	data.Fname = r.FormValue("fname")
 	data.Lname = r.FormValue("lname")
-	log.Println(data.Fname)
-	log.Println(data.Lname)
 	if data.Fname == "124" && data.Lname == "qwe" {
 		http.Redirect(w, r, "/admin?id=oui", http.StatusTemporaryRedirect)
 	} else {
@@ -94,9 +101,32 @@ func (rt *Router) handleClick(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rt *Router) routeHome(w http.ResponseWriter, r *http.Request) {
-	log.Println("ROUTE INDEX")
-	log.Println(r.RequestURI)
-	err := rt.templates.Render(w, "index", rt.count)
+	log.Println("ROUTE HOME")
+	err := rt.templates.Render(w, "home", rt.count)
+	if err != nil {
+		log.Printf("An error occurred while sending HTML file: %s \n", err)
+	}
+}
+
+func (rt *Router) routeBooksContent(w http.ResponseWriter, r *http.Request) {
+	log.Println("ROUTE BOOKS")
+	err := rt.templates.Render(w, "books-content", rt.count)
+	if err != nil {
+		log.Printf("An error occurred while sending HTML file: %s \n", err)
+	}
+}
+
+func (rt *Router) routeHomeContent(w http.ResponseWriter, r *http.Request) {
+	log.Println("ROUTE Home Content")
+	err := rt.templates.Render(w, "home-content", rt.count)
+	if err != nil {
+		log.Printf("An error occurred while sending HTML file: %s \n", err)
+	}
+}
+
+func (rt *Router) routeSeriesContent(w http.ResponseWriter, r *http.Request) {
+	log.Println("ROUTE Series")
+	err := rt.templates.Render(w, "series-content", rt.count)
 	if err != nil {
 		log.Printf("An error occurred while sending HTML file: %s \n", err)
 	}
@@ -105,12 +135,15 @@ func (rt *Router) routeHome(w http.ResponseWriter, r *http.Request) {
 func (rt *Router) routeAdmin(w http.ResponseWriter, r *http.Request) {
 	log.Println("ROUTE ADMIN")
 	if r.URL.Query().Get("id") == "oui" {
-		err := rt.templates.Render(w, "admin", rt.count)
+		err := rt.templates.Render(w, "admin-content", nil)
 		if err != nil {
 			log.Printf("An error occurred while sending HTML file: %s \n", err)
 		}
 		return
 	}
-	http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
+	rt.routeHomeContent(w, r)
+}
 
+func (rt *Router) rerouteToHome(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
 }
